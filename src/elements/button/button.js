d0c4233eed1adb944e8557217750e1386f8c9672
leviathanbeak88 <elvisdedic@outlook.com>
2019-04-07 13:18:51 +0200

    finished button

--- src/elements/button/button.js
+++ src/elements/button/button.js
@@ -2,11 +2,16 @@ import './button.css'
 
 const ButtonComponent = {
 
-  view: () => m('button.default-button', {
-      onclick: event => alert('You clicked me.'),
-    }, 'Generate grid')
-  
+  view: (vnode) => m('button.default-button', {
+      onclick: event => vnode.attrs.onclick(event),
+    }, vnode.attrs.name)  
 
 }
 
 export default ButtonComponent
+
+{/* <button onclick={ function(event) { console.log(event) } } >  click me </button>
+
+function handleMe(event) {
+  console.log(event)
+}  */}
